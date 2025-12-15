54081ec98f7f054f38c8cfa8846b20b554e599e5
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-12-15 15:56:11 -0500

    Working test selenium code

--- main.py
+++ main.py
@@ -1,47 +1,15 @@
-import requests
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-# Cookies
-session = requests.Session()
-
-base_url = "https://nubanner.neu.edu/StudentRegistrationSsb/ssb/"
-
-# Step 1
-session.get(base_url)
-cookies = session.cookies.get_dict()
-
-session_cookie = cookies["JSESSIONID"]
-nubanner_cookie = cookies["nubanner-cookie"]
-
-# Step 2
-reset_response = session.post(
-    f"{base_url}/ssb/classSearch/resetDataForm"
-)
-print(reset_response.status_code)
-
-
-# Step 3
-post_headers = {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Cookie": session_cookie + "; " + nubanner_cookie + ";"
-}
-
-post_data = {
-    "term": "202630",
-    "studyPath": "",
-    "studyPathText": "",
-    "startDatepicker": "",
-    "endDatepicker": ""
-}
-
-
-post = session.post(base_url + "term/search", headers=post_headers, data=post_data)
-print(post.headers)
-print(post.text)
-
-
-# url = "https://nubanner.neu.edu/StudentRegistrationSsb/ssb/searchResults"
-# payload = {"txt_subject": "CS", "txt_courseNumber": "3100", "txt_term": "202630"}
-# response = requests.get(url, params=payload)
-# print(response.url)
-# print(response.text)
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+time.sleep(10)
