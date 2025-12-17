82b6a82b164cf5691b22019d73d86a8e2da8a322
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-12-17 11:55:30 -0500

    Made CourseGetter class for more efficient reads

--- get_course.py
+++ get_course.py
@@ -1,116 +1,127 @@
 from course_sections import Course, Section, Days
 import requests
 
-BASE_URL = "https://nubanner.neu.edu/StudentRegistrationSsb/"
 
-def get_course(session, term: str, course_code: str) -> Course:
-    """Fetches and returns the sections for a given course code and term.
+class CourseGetter:
+    BASE_URL = "https://nubanner.neu.edu/StudentRegistrationSsb/"
 
-    Params
-    ------------
-    session : requests.Session
-        An active requests session to maintain cookies and headers.
-    term : str
-        The term code (e.g., "202630" for Spring 2026).
-    course_code : str
-        The course code (e.g., "CY2550").
+    def __init__(self, session: requests.Session, term: str):
+        self._session = session
+        self._term = term
+        
+        # Manage cookies
+        self._client_cookie = self._session.cookies.get_dict().get("JSESSIONID")
+        self._nubanner_cookie = self._session.cookies.get_dict().get("nubanner-cookie")
 
-    Returns
-    ------------
-    Course
-        An object containing the sections of the specified course.
-    """
-    # Initialize Course object
-    course = Course(course_code)
+        if not self._client_cookie or not self._nubanner_cookie:
+            self._session.get(CourseGetter.BASE_URL)
+            self._client_cookie = self._session.cookies.get_dict().get("JSESSIONID")
+            self._nubanner_cookie = self._session.cookies.get_dict().get("nubanner-cookie")
 
-    # Manage cookies
-    client_cookie = session.cookies.get_dict().get("JSESSIONID")
-    nubanner_cookie = session.cookies.get_dict().get("nubanner-cookie")
+        # Term Declaration
+        term_header = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UT",
+            "Cookie": f"JSESSIONID={self._client_cookie};\
+                            nubanner-cookie={self._nubanner_cookie}"
+        }
+        term_body = {
+            "term": self._term,
+            "studyPath": "",
+            "studyPathText": "",
+            "studyDatepicker": "",
+            "endDatePicker": "",
+        }
+        self._session.post(CourseGetter.BASE_URL + "ssb/term/search?mode=search", 
+                           data=term_body)
 
-    if not client_cookie or not nubanner_cookie:
-        session.get(BASE_URL)
-        client_cookie = session.cookies.get_dict().get("JSESSIONID")
-        nubanner_cookie = session.cookies.get_dict().get("nubanner-cookie")
+    def get_course(self, course_code: str) -> Course:
+        """Fetches and returns the sections for a given course code and term.
 
-    # Reset Request
-    reset_header = {
-        "Cookie": f"JSESSIONID={client_cookie}; nubanner-cookie={nubanner_cookie}"
-    }
-    session.post(BASE_URL + "ssb/classSearch/resetDataForm", headers=reset_header)
+        Params
+        ------------
+        session : requests.Session
+            An active requests session to maintain cookies and headers.
+        term : str
+            The term code (e.g., "202630" for Spring 2026).
+        course_code : str
+            The course code (e.g., "CY2550").
 
-    # Term Declaration
-    term_header = {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UT",
-        "Cookie": f"JSESSIONID={client_cookie}; nubanner-cookie={nubanner_cookie}"
-    }
-    term_body = {
-        "term": term,
-        "studyPath": "",
-        "studyPathText": "",
-        "studyDatepicker": "",
-        "endDatePicker": "",
-    }
-    session.post(BASE_URL + "ssb/term/search?mode=search", data=term_body)
+        Returns
+        ------------
+        Course
+            An object containing the sections of the specified course.
+        """
+        # Initialize Course object
+        course = Course(course_code)
 
-    # Pull Course data
 
-    # Extract subject code and course number
-    subject_code = ""
-    for i, char in enumerate(course_code):
-        if char.isdigit():
-            subject_code = course_code[:i]
-            break
-    course_number = course_code[i:]
+        # Reset Request
+        reset_header = {
+            "Cookie": f"JSESSIONID={self._client_cookie};\
+                        nubanner-cookie={self._nubanner_cookie}"
+        }
+        self._session.post(CourseGetter.BASE_URL + "ssb/classSearch/resetDataForm", headers=reset_header)
 
-    course_header = {
-        "Cookie": f"JSESSIONID={client_cookie}; nubanner-cookie={nubanner_cookie}"
-    }
+        # Pull Course data
 
-    course_params = {
-        "txt_subject": subject_code,
-        "txt_courseNumber": course_number,
-        "txt_term": term,
-    }
-    course_response = session.get(BASE_URL + "ssb/searchResults/searchResults",
-                                  params=course_params, headers=course_header)
+        # Extract subject code and course number
+        subject_code = ""
+        for i, char in enumerate(course_code):
+            if char.isdigit():
+                subject_code = course_code[:i]
+                break
+        course_number = course_code[i:]
 
-    # Parse Course Data
+        course_header = {
+            "Cookie": f"JSESSIONID={self._client_cookie};\
+                        nubanner-cookie={self._nubanner_cookie}"
+        }
 
-    data = course_response.json().get("data", [])
+        course_params = {
+            "txt_subject": subject_code,
+            "txt_courseNumber": course_number,
+            "txt_term": self._term,
+        }
+        course_response = self._session.get(CourseGetter.BASE_URL + "ssb/searchResults/searchResults",
+                                      params=course_params, headers=course_header)
 
-    if data is None:
-        print("No data found for the specified course.")
-        return
+        # Parse Course Data
 
-    for section_data in data:
-        available_slots = section_data.get("seatsAvailable", 0)
-        reference_number = section_data.get("courseReferenceNumber", "")
-        campus = section_data.get("campusDescription", "")
+        data = course_response.json().get("data", [])
 
-        days = []
-        start_time = ""
-        end_time = ""
-        meeting_times = section_data.get("meetingsFaculty", [])
-        if meeting_times:
-            faculty_dict = meeting_times[0]
-            meeting_time = faculty_dict.get("meetingTime", {})
-            for day in Days:
-                if meeting_time.get(day.value, False):
-                    days.append(day)
+        if data is None:
+            print("No data found for the specified course.")
+            return
 
-            start_time = meeting_time.get("beginTime", "")
-            end_time = meeting_time.get("endTime", "")
+        for section_data in data:
+            available_slots = section_data.get("seatsAvailable", 0)
+            reference_number = section_data.get("courseReferenceNumber", "")
+            campus = section_data.get("campusDescription", "")
 
-        if days and start_time and end_time and reference_number and campus:
-            section = Section(
-                reference_number=int(reference_number),
-                campus=campus,
-                days=days,
-                start_time=start_time,
-                end_time=end_time,
-                available_slots=int(available_slots)
-            )
-            course.add_section(section)
+            days = []
+            start_time = ""
+            end_time = ""
+            meeting_times = section_data.get("meetingsFaculty", [])
+            if meeting_times:
+                faculty_dict = meeting_times[0]
+                meeting_time = faculty_dict.get("meetingTime", {})
+                for day in Days:
+                    if meeting_time.get(day.value, False):
+                        days.append(day)
 
-    return course
+                start_time = meeting_time.get("beginTime", "")
+                end_time = meeting_time.get("endTime", "")
+
+            if days and start_time and end_time and reference_number and campus:
+                section = Section(
+                    reference_number=int(reference_number),
+                    campus=campus,
+                    days=days,
+                    start_time=start_time,
+                    end_time=end_time,
+                    available_slots=int(available_slots)
+                )
+                course.add_section(section)
+
+        return course
 
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
-from get_course import get_course
+from get_course import CourseGetter
 
 if __name__ == "__main__":
     with requests.Session() as session:
-        print(get_course(session, "202630", "COMM1210"))
+        course_getter = CourseGetter(session, "202630")
+        course = course_getter.get_course("COMM1210")
+        print(course)
+        course2 = course_getter.get_course("CY2550")
+        print(course2)
+
+        # courses = []
+        # courses.append(get_course(session, "202630", "COMM1210")))
+        # courses.append(get_course(session, "202630", "COMM1210")))
+        # courses.append(get_course(session, "202630", "COMM1210")))
+        # courses.append(get_course(session, "202630", "COMM1210")))
